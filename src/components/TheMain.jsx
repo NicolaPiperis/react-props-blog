882480a4454d46245fc914abc96c08b40b38d848
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-11-30 16:25:59 +0100

    completed

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -1,33 +1,47 @@
+import React from 'react';
 import style from "../css/modules/SquareMain.module.css";
-export default function TheMain () {
-    return(
-        <main className="bg-gray-200 h-[calc(100vh-160px)] flex items-center justify-center">
+import { posts } from "../posts.js";
 
-            <div className= {`${style.square}`}>
+const defaultImg = "/mac_entra.png";
 
-                <div className="h-3/5 bg-gray-400 rounded-t-3xl flex justify-center">
-                    <img className="max-w-full max-h-full" src="/mac_entra.png" alt="#" />
-                </div>
+function TagsList({ tags }) {
+  const tagsColorsMap = {
+    'html': 'bg-amber-600',
+    'css': 'bg-red-500',
+    'js': 'bg-yellow-500',
+    'php': 'bg-violet-500',
+  };
 
-                <div>
-                    <h3 className="p-1 font-semibold">
-                        Mac che urla
-                    </h3>
-                    <p className="p-1">Non può entrare.. 
-                        Entra? 
-                        Non entra! 
-                        Entraaaaaaa? 
-                        Non entraa! 
-                        Entraaaaaaaa? 
-                        Non penso proprio..
-                    </p>
-                    <button className="p-2 bg-yellow-600 text-white m-1 hover:text-black">
-                        Beccati sto Cigno per favore
-                    </button>
-                </div>
+  return tags.map((tag, i) => {
+    const color = tagsColorsMap[tag] ?? "bg-gray-100";
 
-            </div>
-            
-        </main>
+    return (
+      <span key={"tag_" + i}
+        className={"rounded-lg px-2 text-xs " + color}>
+        {tag}
+      </span>
     );
+  });
+}
+
+export default function TheMain() {
+  return (
+    <main className="bg-gray-200 max-h-full pb-40 flex items-center justify-center flex-wrap gap-5">
+      {posts
+        .filter(post => post.published)
+        .map((post, i) => (
+          <div key={i} className={style.square}>
+            <div className="h-3/5 bg-gray-400 rounded-t-3xl flex justify-center">
+              <img className="max-w-full max-h-full" src={post.image || defaultImg} alt={`Post ${i}`} />
+            </div>
+
+            <div>
+              <h3 className="p-1 font-semibold">{post.title}</h3>
+              <p className="p-1">{post.content}</p>
+              <TagsList tags={post.tags} />
+            </div>
+          </div>
+        ))}
+    </main>
+  );
 }
